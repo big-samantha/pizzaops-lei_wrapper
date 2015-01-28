9c07c47a2ee9c34454cbbb728ef3664d8a0e2950
Zachary Alex Stern <zacharyalexstern@gmail.com>
2015-01-27 20:53:39 -0500

    ActiveMQ Hub and Spoke Roles

---
+++ manifests/role/amq_hub.pp
@@ -0,0 +1,4 @@
+class lei_wrapper::amq_hub {
+  include lei_wrapper::common
+  include puppet_enterprise::profile::amq::hub
+}
---
+++ manifests/role/amq_spoke.pp
@@ -0,0 +1,4 @@
+class lei_wrapper::amq_spoke {
+  include lei_wrapper::common
+  include puppet_enterprise::profile::amq::broker
+}
